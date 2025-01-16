743470a9a036f25cc8be2bf947dde31e46fa19b8
Paul-Ndingue <paulndingue027@gmail.com>
2025-01-16 15:59:53 -0500

    creation du footer

---
+++ components/Footer.jsx
@@ -0,0 +1,11 @@
+
+import styles from './Footer.module.css';
+
+export default function Footer() {
+    return(
+    <footer className={styles.footer}>
+        &copy;2025 SPECTACLE DIRECT  
+        CGU
+    </footer>
+    )
+}

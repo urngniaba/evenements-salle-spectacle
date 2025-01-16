1a218cee12192f7c15ed5ebfa52a71cc8672113c
Ulrich-Romeo-Ngniaba <cheriepassy2011@gmail.com>
2025-01-16 15:59:27 -0500

    Creation du header

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,5 +1,10 @@
+import styles from "@/components/Header.module.css"
+
 export default function Header(){
     return (
-        <></>
+        <header>
+            <span>SPECTACLE EN DIRECT</span>
+            <span>SPECTACLE EN DIRECT</span>
+        </header>
     )
 }

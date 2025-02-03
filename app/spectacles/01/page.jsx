b6e4f668b4ab361cda1a1a66787492a4428f630e
Ulrich-Romeo-Ngniaba <cheriepassy2011@gmail.com>
2025-02-03 12:08:56 -0500

    Correction du margin de trop sur le header

--- app/spectacles/01/page.jsx
+++ app/spectacles/01/page.jsx
@@ -5,14 +5,18 @@ import logo1 from '@/public/imageAffichage/concert.jpg';
 
 export default function Spectacle01() {
     return <>
-        <Card titre='Balle de danse'
-            categorie='Ambiance festif'
-            sous_categorie='Musical'
-            description='Soiree dansante (Parade, défilé)'
-            bouton='Faire une reservation' 
-            route='#'
-            imageSrc={logo1}>
-        </Card>
+        <div className={styles.spectacle}>
+
+            <h1>BALLE DE DANSE</h1>
+            <Card titre='Balle de danse'
+                categorie='Ambiance festif'
+                sous_categorie='Musical'
+                description='Soiree dansante (Parade, défilé)'
+                bouton='Faire une reservation'
+                route='#'
+                imageSrc={logo1}>
+            </Card>
+        </div>
 
     </>
 }

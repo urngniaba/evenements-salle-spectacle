1cb08d05705be961307d80aedbeb7c9bdd4cfa12
Ulrich-Romeo-Ngniaba <cheriepassy2011@gmail.com>
2025-02-23 11:48:20 -0500

    Design de Couleurs de la banniere

--- components/Banniere.jsx
+++ components/Banniere.jsx
@@ -4,17 +4,19 @@ export default function Banniere() {
     // La partie contenant la grande image juste en dessous de la barre de mennu sur la page d'accueil
     return <div className={styles.banniere}>
         <h1>SPECTACLE EN DIRECT, LE SPECTACLE EN TOUTE LIBERTE</h1>
-        <div className={styles.cadre_jaune}>
+        <div>
             L'annuaire des spectacles pour les collectivités
         </div>
-        <form action="" className={styles.zone_recherche}>
+        <form action="">
             <input type="text" placeholder="Que recherchez-vous ?" />
             <input type="text" placeholder="Où ?" />
             <input type="date" />
             <button type="button">Rechercher</button>
         </form>
         <p>
-            Passez en direct ou faites vous accompagner ! <br />
+            Passez en direct ou faites vous accompagner !
+        </p>
+        <p>
             C'est vous qui décidez !
         </p>
     </div>

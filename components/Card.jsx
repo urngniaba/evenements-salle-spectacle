93a4f935f0d3b4dc09116b310ddcb0c11d9e7c42
Ulrich-Romeo-Ngniaba <cheriepassy2011@gmail.com>
2025-02-23 13:17:09 -0500

    Design couleur de la partie affichage

--- components/Card.jsx
+++ components/Card.jsx
@@ -6,13 +6,13 @@ import Image from 'next/image';
 export default function Card({titre, categorie, sous_categorie, description, bouton, route, imageSrc }) {
     return (
         <div className={styles.card}>
-            <Image src={imageSrc} alt="card" className={styles.card_image} />
+            <Image src={imageSrc} alt="card"/>
             <h3>{titre}</h3>
             <div>
                 <span>{categorie}</span>
                 <span>{sous_categorie}</span>
                 <span>{description}</span>
-                <Link href={route} className={styles.link}><button>{bouton}</button></Link> 
+                <Link href={route}><button>{bouton}</button></Link> 
             </div>
         </div>
     )

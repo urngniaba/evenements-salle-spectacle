61d6ff1673097c682067ecf8016598102a86ca36
Ulrich-Romeo-Ngniaba <cheriepassy2011@gmail.com>
2025-02-24 13:21:54 -0500

    Gestion de la responsivite des pages a propos et prestataire

--- components/PrestationVente.jsx
+++ components/PrestationVente.jsx
@@ -1,15 +1,13 @@
 import styles from "@/components/PrestationVente.module.css"
 
 // Composant representant une etape (cadre en bleue avec les numero dans un cercle) des vente des prestations
-export default function PrestionVente({children, numero}) {
-    return (
-        <div className={styles.prestation_vente_etape}>
-            <div className={styles.numero}>
-                <span>{numero}</span>
-            </div>
-            <p>
-                {children}
-            </p>
+export default function PrestionVente({ children, numero }) {
+    return <div className={styles.prestation_vente_etape}>
+        <div className={styles.numero}>
+            <span>{numero}</span>
         </div>
-    )
+        <p>
+            {children}
+        </p>
+    </div>
 }

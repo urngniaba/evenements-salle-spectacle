8af245b0335e7562bfc5ef35b639f159d1a4a9bb
Ulrich-Romeo-Ngniaba <cheriepassy2011@gmail.com>
2025-03-10 20:57:08 -0400

    Gestion des images de prestataire

--- components/Banniere.jsx
+++ components/Banniere.jsx
@@ -1,16 +1,11 @@
 import styles from "@/components/Banniere.module.css"
-import styles2 from "@/components/BackgroundImageWithText.module.css";
-import BackgroundImage from "./BackgroundImage";
-import image from "@/public/small-banniere.webp";
-import BackgroundImageWithText from "./BackgroundWithText";
-
 
 // La partie contenant la grande image juste en dessous de la barre de mennu sur la page d'accueil
 export default function Banniere() {
-    return <div className={`${styles.banniere}`}>
-        <BackgroundImage src={image} alt="Spectacle"/>
+    return <div className={styles.banniere}>
+        <h1>Vivez l’instant suspendu, le spectacle libre et vivant</h1>
         <div>
-            <h1>Vivez l’instant suspendu, le spectacle libre et vivant</h1>
+            Le calendrier des arts vivants pour les sociétés
         </div>
         <form action="" noValidate>
             <input type="text" placeholder="Que recherchez-vous ?" id="key-word" />
@@ -18,5 +13,11 @@ export default function Banniere() {
             <input type="date" id="event-date" placeholder="mm/dd/yyyy" />
             <button type="button">Rechercher</button>
         </form>
+        <p>
+            Lancez-vous en live ou faites-vous épauler !
+        </p>
+        <p>
+            C’est à vous de choisir !
+        </p>
     </div>
 }

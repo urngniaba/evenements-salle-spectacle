db12d359d08b8d17a960db6d92fbc2c8b203424c
Ulrich-Romeo-Ngniaba <cheriepassy2011@gmail.com>
2025-03-11 13:55:23 -0400

    Retrait de la partie qui verifie le compte utilisateur dans la page contact

--- actions/contactServeur.js
+++ actions/contactServeur.js
@@ -12,32 +12,32 @@ export async function contactServeur(formData) {
   }
 
   //   On verifie s'il existe un compte avec ce courriel
-  const email = formData.get("email");
-  const emails = accounts.map((account) => account.email);
-  if (!emails.includes(email)) {
-    erreur = true;
-    newState.email.erreur = "Adresse courriel introuvable";
-    return [erreur, newState];
-  }
+  // const email = formData.get("email");
+  // const emails = accounts.map((account) => account.email);
+  // if (!emails.includes(email)) {
+  //   erreur = true;
+  //   newState.email.erreur = "Adresse courriel introuvable";
+  //   return [erreur, newState];
+  // }
 
   //   On verifie s'il y a un compte avec ce numéro
-  const telephone = formData.get("telephone");
-  const telephones = accounts.map((account) => account.telephone);
-  if (!telephones.includes(telephone)) {
-    erreur = true;
-    newState.telephone.erreur = "Numéro de téléphone introuvable";
-    return [erreur, newState];
-  }
+  // const telephone = formData.get("telephone");
+  // const telephones = accounts.map((account) => account.telephone);
+  // if (!telephones.includes(telephone)) {
+  //   erreur = true;
+  //   newState.telephone.erreur = "Numéro de téléphone introuvable";
+  //   return [erreur, newState];
+  // }
 
   //   On verifie si le courriel et le numéro sont pour le même compte
-  const isValidAccount = accounts.some(
-    (account) => account.email === email && account.telephone === telephone
-  );
-  if (!isValidAccount) {
-    erreur = true;
-    newState.email.erreur = "Compte utilisateur invalide";
-    return [erreur, newState];
-  }
+  // const isValidAccount = accounts.some(
+  //   (account) => account.email === email && account.telephone === telephone
+  // );
+  // if (!isValidAccount) {
+  //   erreur = true;
+  //   newState.email.erreur = "Compte utilisateur invalide";
+  //   return [erreur, newState];
+  // }
 
   //   Tout est correct
 
